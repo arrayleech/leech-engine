e8b880d7bfd743cf6ef4a430edf960eee1e5360b
arrayleech <arrayleech@proton.me>
2024-08-18 19:26:07 +0200

    Creating the shaders

--- examples/Example.c
+++ examples/Example.c
@@ -8,6 +8,26 @@ const unsigned int WINDOW_WIDTH  = 800;
 const unsigned int WINDOW_HEIGHT = 600;
 const char*        WINDOW_TITLE  = "Leech Engine";
 
+// Shader Source Codes
+const char* vertexShaderSource =
+  "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "layout (location = 1) in vec3 aCol;\n"
+  "out vec3 vertCol;\n"
+  "void main()\n"
+  "{\n"
+  "  vertCol = aCol;\n"
+  "  gl_Position = vec4(aPos, 1.f);\n"
+  "}\0";
+const char* fragmentShaderSource =
+  "#version 330 core\n"
+  "in vec3 vertCol;\n"
+  "out vec4 FragColor;\n"
+  "void main()\n"
+  "{\n"
+  "  FragColor = vec4(vertCol, 1.f);\n"
+  "}\0";
+
 int main()
 {
   // Initializing GLFW
@@ -49,16 +69,69 @@ int main()
   GLclampf alpha = 1.0f;
   glClearColor(red, green, blue, alpha);
 
+  // Vertex and Fragment Shaders
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+  glCompileShader(vertexShader);
+  glCompileShader(fragmentShader);
+
+  // Info Log
+  int success = 0;
+  char infoLog[256];
+
+  // Validating the Vertex Shader
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 256, NULL, infoLog);
+    fprintf(stderr, "Failed to compile the vertex shader!\n");
+    fprintf(stderr, "Error:\n%s", infoLog);
+  }
+
+  // Validating the Fragment Shader
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(fragmentShader, 256, NULL, infoLog);
+    fprintf(stderr, "Failed to compile the fragment shader!\n");
+    fprintf(stderr, "Error:\n%s", infoLog);
+  }
+
+  // Shader Program
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  // Validating the Shader Program
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetProgramInfoLog(shaderProgram, 256, NULL, infoLog);
+    fprintf(stderr, "Failed to link the shader program!\n");
+    fprintf(stderr, "Error:\n%s", infoLog);
+  }
+
+  // Deleting the Shaders
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
   printf("Leech Engine\n");
 
   // Main Loop
   while (!glfwWindowShouldClose(glfwWindow))
   {
     glfwPollEvents();
+    glUseProgram(shaderProgram);
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(glfwWindow);
   }
 
+  glDeleteProgram(shaderProgram);
   glfwDestroyWindow(glfwWindow);
   glfwTerminate();
   return EXIT_SUCCESS;
